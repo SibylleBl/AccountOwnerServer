27cd9100d50f12fe0f38d062785a2938e157eb58
Sibylle Blondeau <sibylle.blondeau@hotmail.fr>
2023-06-14 13:14:20 +0200

    base

---
+++ AccountOwnerServer/Program.cs
@@ -0,0 +1,39 @@
+using AccountOwnerServer.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// J'appelle les configurations serveurs que j'ai définie dans Extensions.
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+
+else {
+    app.UseHsts();    
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
